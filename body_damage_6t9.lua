85289d75b165c27afa3ec6ddd3a0023ddb51f5a7
kalu4136b <kalharalakshitha25@gmail.com>
2025-03-01 20:27:13 +0530

    updated

--- body_damage_6t9.lua
+++ body_damage_6t9.lua
@@ -12,16 +12,16 @@ local bodyParts = {
     RIGHT_LEG = 51826
 }
 
--- Define additional damage multipliers for body parts(you can edit this)
+-- Define additional damage multipliers for body parts
 local damageMultipliers = {
-    [bodyParts.HEAD] = 2.0, -- head(2X DAMAGE)
-    [bodyParts.NECK] = 1.0, -- neek
-    [bodyParts.UPPER_BODY] = 1.5, --chest
-    [bodyParts.LOWER_BODY] = 1.0, --body
-    [bodyParts.LEFT_ARM] = 0.7, --left hand
-    [bodyParts.RIGHT_ARM] = 0.7, -- left hand
-    [bodyParts.LEFT_LEG] = 0.5, --left leg
-    [bodyParts.RIGHT_LEG] = 0.5 --right leg
+    [bodyParts.HEAD] = 2.0, -- head (2x damage)
+    [bodyParts.NECK] = 1.0, -- neck
+    [bodyParts.UPPER_BODY] = 1.5, -- chest
+    [bodyParts.LOWER_BODY] = 1.0, -- lower body
+    [bodyParts.LEFT_ARM] = 0.7, -- left arm
+    [bodyParts.RIGHT_ARM] = 0.7, -- right arm
+    [bodyParts.LEFT_LEG] = 0.5, -- left leg
+    [bodyParts.RIGHT_LEG] = 0.5 -- right leg
 }
 
 -- Event handler for when a player is damaged
@@ -38,17 +38,12 @@ AddEventHandler('gameEventTriggered', function(eventName, eventData)
             local damageMultiplier = damageMultipliers[boneIndex] or 1.0
 
             -- Apply additional damage based on the body part
-            local damage = GetEntityHealth(victim)
-            SetEntityHealth(victim, damage - (damage * damageMultiplier))
+            local baseDamage = 10 -- Example base damage
+            local damage = baseDamage * damageMultiplier
+            SetEntityHealth(victim, GetEntityHealth(victim) - damage)
 
             -- Debug output (optional)
-            print(string.format("Player hit in body part: %d, Damage multiplier: %.2f", boneIndex, damageMultiplier))
+            print(string.format("Player hit in body part: %d, Damage multiplier: %.2f, Damage dealt: %.2f", boneIndex, damageMultiplier, damage))
         end
     end
 end)
-
--- Function to get the last damaged bone of a ped
-function GetPedLastDamageBone(ped)
-    local boneIndex = GetPedLastDamageBone(ped)
-    return boneIndex
-end
